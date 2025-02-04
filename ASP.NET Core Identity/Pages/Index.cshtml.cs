cfa552e803744ba81921790501bf3e08463b9439
Xavier2929 <castilloxavier49@gmail.com>
2025-02-03 17:03:22 -0700

    added the authorize tag on the index page, this wil autoredirect to the login page , this denies any anonymouse access to the login page.

--- ASP.NET Core Identity/Pages/Index.cshtml.cs
+++ ASP.NET Core Identity/Pages/Index.cshtml.cs
@@ -1,9 +1,11 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
 
 namespace ASP.NET_Core_Identity.Pages
 {
+    [Authorize]
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
